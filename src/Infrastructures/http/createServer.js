e96d4288b3245c608514a5bfe26b71891e5bb063
Indi Paradise <indiparadise3@gmail.com>
2023-12-25 17:59:57 +0700

    ga error

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -56,7 +56,13 @@ const createServer = async (container) => {
     },
   ]);
 
-
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello world!',
+    }),
+  });
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
